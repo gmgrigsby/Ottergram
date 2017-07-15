d68ddb17ca84703c0656752aff232c7a04ea2387
Glen Grigsby <gmgrigsby@gmail.com>
2017-07-15 03:16:57 -0500

    began adding js code, minor css changes, removed extra list items

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageURL, titleText)	{
+	'use strict';
+	var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+	detailImage.setAttribute('src', imageURL);
+	var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+	detailTitle.textContent = titleText;
+}
